45826984f986fe165b0956accf67f6c9430167dd
Davenn69 <devendeven98@gmail.com>
2026-02-19 13:07:37 +0700

    added : login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardContent,
@@ -5,49 +7,90 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, Form, useForm } from "react-hook-form";
 import { Separator } from "@/components/ui/separator";
 import AppButton from "@/lib/cores/components/button";
 import TextField from "@/lib/cores/components/custom_text_field";
 import { Gap } from "@/lib/cores/components/gap";
 import { textTheme } from "@/lib/cores/constants/textTheme";
 import { buttonVariants } from "@/lib/cores/types/buttonTypes";
-import { useAuth } from "../../../lib/features/login/(viewmodel)/auth_viewmodel";
-export default function LoginPage() {
-  const useLogin = useAuth();
-  return (
-    <div className="w-full h-full flex content-center justify-center">
-      <Gap value="pt-4" />
-      <Card className="border border-gray2 px-12 py-4">
-        <CardHeader className="flex justify-center items-center pt-4">
-          <CardTitle className={textTheme.heading1}>
-            Login to vaulttech
-          </CardTitle>
-          <CardDescription>Sign in now to start</CardDescription>
-        </CardHeader>
+import { useLoginFunction } from "../../../lib/features/login/viewmodel/loginVM";
+import PageWrapper from "@/lib/cores/components/page_wrapper";
 
-        <CardContent className="w-full flex flex-col">
-          <TextField id="email" label="Email" hint="Enter your email" />
-          <Gap value="pt-4"></Gap>
-          <TextField
-            id="password"
-            label="Password"
-            hint="Enter your password"
-          />
-          <Gap value="pt-8" />
-          <AppButton label="Login" />
-          <Gap value="pt-4" />
-          <p>
-            Forgot your password? <a href="">click here</a>
-          </p>
-          <Gap value="pt-4" />
-          <Separator
-            orientation="horizontal"
-            className="border-gray2 border-t"
-          />
-          <Gap value="pt-8" />
-          <AppButton label="Continue with Google" variant="gray" />
-        </CardContent>
-      </Card>
-    </div>
+const formSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .regex(/[A-Z]/, "At least one uppercase letter")
+    .regex(/[0-9]/, "At least one number"),
+});
+
+export default function LoginPage() {
+  const { onSubmit, isLoading, error } = useLoginFunction();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  return (
+    <PageWrapper isLoading={isLoading}>
+      <div className="w-full h-full flex content-center justify-center">
+        <Gap value="pt-4" />
+        <Card className="border border-gray2 px-12 py-4">
+          <CardHeader className="flex justify-center items-center pt-4">
+            <CardTitle className={textTheme.heading1}>
+              Login to vaulttech
+            </CardTitle>
+            <CardDescription>Sign in now to start</CardDescription>
+          </CardHeader>
+
+          <CardContent className="w-full flex flex-col">
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <Controller
+                name="email"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <TextField
+                    {...field}
+                    id="email"
+                    label="Email"
+                    hint="Enter your email"
+                    error={fieldState.error?.message}
+                  />
+                )}
+              />
+              <Gap value="pt-4"></Gap>
+              <Controller
+                name="password"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <TextField
+                    {...field}
+                    id="password"
+                    label="Password"
+                    isPassword={true}
+                    hint="Enter your password"
+                    error={fieldState.error?.message}
+                  />
+                )}
+              />
+              <Gap value="pt-8" />
+              <AppButton label="Login" type="submit" />
+              <Gap value="pt-4" />
+              {/* <p>
+              Forgot your password? <a href="">click here</a>
+            </p> */}
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </PageWrapper>
   );
 }

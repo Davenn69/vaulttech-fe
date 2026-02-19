8fceae7e42c20dd9d42c7e59959a0d9f514eb5c8
Davenn69 <devendeven98@gmail.com>
2026-02-19 09:04:00 +0700

    modified : font paths

--- lib/cores/constants/fonts.ts
+++ lib/cores/constants/fonts.ts
@@ -3,37 +3,37 @@ import localFont from "next/font/local";
 export const font = localFont({
   src: [
     {
-      path: "../../public/assets/fonts/Manrope-ExtraLight.ttf",
+      path: "../../../public/assets/fonts/Manrope-ExtraLight.ttf",
       weight: "200",
       style: "normal",
     },
     {
-      path: "../../public/assets/fonts/Manrope-Light.ttf",
+      path: "../../../public/assets/fonts/Manrope-Light.ttf",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../../public/assets/fonts/Manrope-Regular.ttf",
+      path: "../../../public/assets/fonts/Manrope-Regular.ttf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../../public/assets/fonts/Manrope-Medium.ttf",
+      path: "../../../public/assets/fonts/Manrope-Medium.ttf",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../../public/assets/fonts/Manrope-SemiBold.ttf",
+      path: "../../../public/assets/fonts/Manrope-SemiBold.ttf",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../../public/assets/fonts/Manrope-Bold.ttf",
+      path: "../../../public/assets/fonts/Manrope-Bold.ttf",
       weight: "700",
       style: "normal",
     },
     {
-      path: "../../public/assets/fonts/Manrope-ExtraBold.ttf",
+      path: "../../../public/assets/fonts/Manrope-ExtraBold.ttf",
       weight: "800",
       style: "normal",
     },

ff47e878bb9a24da1e30ad270aa8367d12dc1ad7
Davenn69 <devendeven98@gmail.com>
2026-02-19 09:07:23 +0700

    added : field prop

--- lib/cores/components/custom_text_field.tsx
+++ lib/cores/components/custom_text_field.tsx
@@ -6,8 +6,9 @@ import {
   FieldSet,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { InputHTMLAttributes } from "react";
 
-type TextFieldProps = {
+type TextFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   id: string;
   label?: string;
   hint?: string;
@@ -15,12 +16,20 @@ type TextFieldProps = {
   error?: string;
 };
 
-const TextField = ({ id, label, hint, onSubmit, error }: TextFieldProps) => {
+const TextField = ({
+  id,
+  label,
+  hint,
+  onSubmit,
+  error,
+  ...field
+}: TextFieldProps) => {
   return (
     <FieldSet className="w-full">
       <FieldGroup className="gap-1">
         {label ? <FieldLabel>{label}</FieldLabel> : null}
         <Input
+          {...field}
           id={id}
           placeholder={hint}
           className="p-4 rounded-10 w-full bg-gray3 border-gray2 focus:border-gray5 text-white"
